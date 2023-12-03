ef4f90b3c1a01e85ec25246cb4a1bb1091325e73
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 14:56:47 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -23,7 +23,7 @@ with open('settings.json', 'w') as f:
     json.dump(location , f)
     API = 'e1313973fe262c3c18b4500d98fe65eb'
     url=f"https://api.openweathermap.org/data/2.5/weather?appid={API}&q={location}&units={unit_chosen}"
-    weatherzone= rq.get(url)
+    weatherzone= requests.get(url)
     response_weatherzone=weatherzone.json()
     json.dump(response_weatherzone , f)
 
