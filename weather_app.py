cab5ed531bd633e114ba91407cd7349545c17c66
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 12:15:43 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -4,20 +4,20 @@ import streamlit as st
 
 api_key = 'e1313973fe262c3c18b4500d98fe65eb'
 
-st.title("Weather Forecaster")
+st.title("Today'\s Weather")
 st.write("## *Made by Laurette*")
 st.write("##")
 
-st.write("### Enter the city name, choose a Temperature unit and a graph type from the bottom:")
+city= st.write("### Enter the city name, choose a Temperature unit and a graph type from the bottom:")
 
-city = st.text_input("Name of The City :", "")
+city_chosen = st.selectbox("Name of The City :", ('jerusalem','city')
 units = st.selectbox("Select Temperature Unit: ", ('celsius', 'fahrenheit'))
 graph = st.selectbox("Select Graph Type:", ('Bar Graph', 'Line Graph'))
 
 
 
 
-url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
+url = f"https://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units={units}"
 
 response = requests.get(url)
 
