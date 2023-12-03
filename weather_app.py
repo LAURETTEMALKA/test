e7867a0c8e2ffeb5098b78a9d02013a8650e1a80
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 14:10:29 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -13,12 +13,6 @@ city_by_default = st.text_input("What is your default location? ")
 city_chosen = st.text_input("Name of The City :") or city_by_default
 units = st.selectbox("Select Temperature Unit: ", ('celsius', 'fahrenheit'))
 
-
-API = 'e1313973fe262c3c18b4500d98fe65eb'
-url=f"https://api.openweathermap.org/data/2.5/weather?appid={API}&q={location}&units={units_chosen}"
-weatherzone= rq.get(url)
-response_weatherzone=weatherzone.json()
-
 with open('settings.json', 'w') as f:
     default_location = input('Your default location: ')
     location = user_input
@@ -26,3 +20,10 @@ with open('settings.json', 'w') as f:
         location = default_location
     json.dump(location , f)
     json.dump(response_weatherzone , f)
+
+
+API = 'e1313973fe262c3c18b4500d98fe65eb'
+url=f"https://api.openweathermap.org/data/2.5/weather?appid={API}&q={location}&units={units_chosen}"
+weatherzone= rq.get(url)
+response_weatherzone=weatherzone.json()
+
