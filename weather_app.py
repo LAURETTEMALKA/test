4fed69a8d3ec6a79c544293e4fdd3b4f64dcfb1b
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 15:50:18 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -10,10 +10,23 @@ st.write("##")
 st.write("### Enter the city name, choose a Temperature unit and a graph type from the bottom:")
 
 with open('settings.json', 'w') as f:
+    options = ['marseille', 'lyon', 'jerusalem', 'paris', 'tel-aviv', 'natanya', 'london', 'new york']
+    list_of_favorites_locations = []
+    st.write("you have to select 5 favorite location in this list")
+    choice1= st.selectbox('Your choice 1 : ',options)
+    list_of_favorites_locations.append(choice1)
+    choice2= st.selectbox('Your choice 2 : ',options)
+    list_of_favorites_locations.append(choice2)
+    choice3= st.selectbox('Your choice 3 : ',options)
+    list_of_favorites_locations.append(choice3)
+    choice4= st.selectbox('Your choice 4 : ',options)
+    list_of_favorites_locations.append(choice4)
+    choice5= st.selectbox('Your choice 5 : ',options)
+    list_of_favorites_locations.append(choice5)
     default_location = st.text_input('Your default location: ')
     if default_location:
         st.write(f'Your location by default is: {default_location}')
-    location = st.text_input('What is you actual location?')
+    location = st.selectbox('What is you actual location?',list_of_favorites_locations)
     if location == "":
         location = default_location
         st.write(f'Your location is: {default_location}')
@@ -27,26 +40,4 @@ with open('settings.json', 'w') as f:
     response_weatherzone=weatherzone.json()
     json.dump(response_weatherzone , f)
 
-
-options = ['marseille', 'lyon', 'jerusalem', 'paris', 'tel-aviv', 'natanya', 'london', 'new york']
-#user_input = ''
-#input_message= st.selectbox("Pick an option in this list of locations:\n",options)
-
-
-list_of_favorites_locations = []
-st.write("you have to select 5 favorite location in this list")
-
-choice1= st.selectbox('Your choice 1 : ',options)
-list_of_favorites_locations.append(choice1)
-choice2= st.selectbox('Your choice 2 : ',options)
-list_of_favorites_locations.append(choice2)
-choice3= st.selectbox('Your choice 3 : ',options)
-list_of_favorites_locations.append(choice3)
-choice4= st.selectbox('Your choice 4 : ',options)
-list_of_favorites_locations.append(choice4)
-choice5= st.selectbox('Your choice 5 : ',options)
-list_of_favorites_locations.append(choice5)
-
-if choice5:
-    st.write("Your favorite locations are:", list_of_favorites_locations)
 
