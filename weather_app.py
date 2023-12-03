15a40507a40d0d332f90427cf8b2517fb0abcd89
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 12:20:12 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -11,7 +11,7 @@ st.write("##")
 city= st.write("### Enter the city name, choose a Temperature unit and a graph type from the bottom:")
 
 city_chosen = st.selectbox("Name of The City :", ('jerusalem','city')
-units = st.selectbox("Select Temperature Unit: ", ('celsius', 'fahrenheit'))
+#units = st.selectbox("Select Temperature Unit: ", ('celsius', 'fahrenheit'))
 graph = st.selectbox("Select Graph Type:", ('Bar Graph', 'Line Graph'))
 
 
