74ae2ef1996d77c9da033acb5799a3785530f529
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 15:43:44 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -50,30 +50,3 @@ list_of_favorites_locations.append(choice5)
 if choice5:
     st.write("Your favorite locations are:", list_of_favorites_locations)
 
-
-for i in range(4):
-    input_message = f'Your choice {i+2} : '
-    user_input = st.text_input(input_message)
-    while user_input.lower() not in options:
-        print(f'"{user_input}" not in the list')
-        user_input = st.text_input(input_message)
-    list_of_favorites_locations.append(user_input)
-    
-with open('settings.json', 'w') as f:
-    default_location = st.text_input('Your default location: ')
-    location = user_input
-    if location == "":
-        location = default_location
-    json.dump(location , f)
-    json.dump(response_weatherzone , f)
-list_of_favorites_locations = []
-
-city_by_default = st.text_input("What is your default location? ")
-city_chosen = st.selectbox("Name of The City :", list_of_favorites_locations) or city_by_default
-units = st.selectbox("Select Temperature Unit: ", ('celsius', 'fahrenheit'))
-
-API = 'e1313973fe262c3c18b4500d98fe65eb'
-url=f"https://api.openweathermap.org/data/2.5/weather?appid={API}&q={location}&units={units_chosen}"
-weatherzone= rq.get(url)
-response_weatherzone=weatherzone.json()
-
