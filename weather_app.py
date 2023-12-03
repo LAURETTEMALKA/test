b1c5b385f72b95e85de241a3c936805e09b5cd38
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 11:51:13 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -1,9 +1,10 @@
 
 import requests
+import streamlit as st
 
 api_key = 'e1313973fe262c3c18b4500d98fe65eb'
 
-city = input('Enter city name: ')
+city = st.write(input('Enter city name: '))
 
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
