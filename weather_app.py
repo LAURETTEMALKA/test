6483ad5ecde63f479228a8b482f6dd42dfb9a936
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 14:23:48 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -20,7 +20,7 @@ list_of_favorites_locations = []
 
 options = ['marseille', 'lyon', 'jerusalem', 'paris', 'tel-aviv', 'natanya', 'london', 'new york']
 user_input = ''
-inpu_message= st.text_input("Pick an option in this list of locations:\n")
+input_message= st.text_input("Pick an option in this list of locations:\n")
 
 for index, item in enumerate(options):
     input_message += f'{index+1}) {item}\n'
@@ -43,6 +43,15 @@ for i in range(4):
     list_of_favorites_locations.append(user_input)
     
 print(list_of_favorites_locations)
+
+with open('settings.json', 'w') as f:
+    default_location = st.text_input('Your default location: ')
+    location = user_input
+    if location == "":
+        location = default_location
+    json.dump(location , f)
+    json.dump(response_weatherzone , f)
+list_of_favorites_locations = []
 
 city_by_default = st.text_input("What is your default location? ")
 city_chosen = st.selectbox("Name of The City :", list_of_favorites_locations) or city_by_default
