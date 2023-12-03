ad9fce5accf38a9b26be05332cb6eb92ccaae834
LAURETTEMALKA <101649987+LAURETTEMALKA@users.noreply.github.com>
2023-12-03 14:42:59 +0100

    Update weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -9,6 +9,13 @@ st.write("##")
 
 st.write("### Enter the city name, choose a Temperature unit and a graph type from the bottom:")
 
+with open('settings.json', 'w') as f:
+    default_location = st.text_input('Your default location: ')
+    location = st.text-input('What is you actual location?')
+    if location == "":
+        location = default_location
+    json.dump(location , f)
+    json.dump(response_weatherzone , f)
 options = ['marseille', 'lyon', 'jerusalem', 'paris', 'tel-aviv', 'natanya', 'london', 'new york']
 #user_input = ''
 #input_message= st.selectbox("Pick an option in this list of locations:\n",options)
@@ -31,11 +38,6 @@ list_of_favorites_locations.append(choice5)
 if choice5:
     st.write("Your favorite locations are:", list_of_favorites_locations)
 
-while user_input.lower() not in options:
-    print(f'"{user_input}" not in the list')
-    user_input = st.text_input(input_message)
-    list_of_favorites_locations.append(user_input)
-
 
 for i in range(4):
     input_message = f'Your choice {i+2} : '
@@ -45,8 +47,6 @@ for i in range(4):
         user_input = st.text_input(input_message)
     list_of_favorites_locations.append(user_input)
     
-print(list_of_favorites_locations)
-
 with open('settings.json', 'w') as f:
     default_location = st.text_input('Your default location: ')
     location = user_input
